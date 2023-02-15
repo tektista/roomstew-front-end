71905cd2f67930ca3d065ef97c4d94abb4189e95
tektista <jnl.bautista.cs@gmail.com>
2023-02-15 16:38:19 +0000

    add basic structure for listing properties to ListingDetails screen

--- roomstew/screens/ListingDetailsScreen.js
+++ roomstew/screens/ListingDetailsScreen.js
@@ -1,11 +1,13 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, View, ScrollView } from "react-native";
 import React, { useState, useEffect } from "react";
 
 import axios from "axios";
 
+import colors from "../config/colors";
 import AppText from "../components/AppText";
 import ListItem from "../components/ListItem";
-import colors from "../config/colors";
+import ListItemSeparator from "../components/ListItemSeparator";
+import Icon from "../components/Icon";
 
 export default function ListingDetailsScreen({ route }) {
   const listing = route.params.item;
@@ -14,6 +16,51 @@ export default function ListingDetailsScreen({ route }) {
   //state of object pulled from axios
 
   const [listingFromDB, setListingFromDB] = useState({});
+
+  const listingItems = [
+    {
+      title: "Rooms",
+      icon: {
+        name: "bed",
+        backgroundColor: colors.primary,
+      },
+    },
+    {
+      title: "Furnished",
+      icon: {
+        name: "table-chair",
+        backgroundColor: colors.primary,
+      },
+    },
+    {
+      title: "Bills",
+      icon: {
+        name: "home-lightning-bolt",
+        backgroundColor: colors.primary,
+      },
+    },
+    {
+      title: "Internet",
+      icon: {
+        name: "wifi",
+        backgroundColor: colors.primary,
+      },
+    },
+    {
+      title: "Living room",
+      icon: {
+        name: "sofa",
+        backgroundColor: colors.primary,
+      },
+    },
+    {
+      title: "Bathrooms",
+      icon: {
+        name: "toilet",
+        backgroundColor: colors.primary,
+      },
+    },
+  ];
 
   //we need this to get the listing details from the server once the listing is clicked
   const getListingDetails = async () => {
@@ -33,29 +80,44 @@ export default function ListingDetailsScreen({ route }) {
   }, []);
 
   return (
-    <View>
+    <ScrollView>
       <Image style={styles.image} source={require("../assets/apartment.jpg")} />
 
-      <View style={styles.detailsContainer}>
+      <View style={styles.userContainer}>
+        <ListItem
+          image={require("../assets/apartment.jpg")}
+          title="Mosh Hamedani"
+          subTitle="5 Listings"
+        ></ListItem>
+      </View>
+
+      <View style={styles.titleContainer}>
         <AppText style={styles.title}>
           {listingFromDB.title} {listingFromDB.listing_id}
         </AppText>
 
-        {/* <AppText style={styles.price}>£{listing.minRoomRent} </AppText> */}
-
         <AppText styles={styles.description}>
           {listingFromDB.description}
         </AppText>
-
-        <View style={styles.userContainer}>
-          <ListItem
-            image={require("../assets/apartment.jpg")}
-            title="Mosh Hamedani"
-            subTitle="5 Listings"
-          ></ListItem>
-        </View>
       </View>
-    </View>
+
+      <View style={styles.detailsContainer}>
+        {listingItems.map((item) => (
+          <View key={item.title}>
+            <ListItem
+              title={item.title}
+              IconComponent={
+                <Icon
+                  name={item.icon.name}
+                  backgroundColor={item.icon.backgroundColor}
+                />
+              }
+            />
+            <ListItemSeparator />
+          </View>
+        ))}
+      </View>
+    </ScrollView>
   );
 }
 
@@ -65,24 +127,25 @@ const styles = StyleSheet.create({
     height: 300,
   },
 
-  detailsContainer: {
+  userContainer: {
+    marginTop: 10,
+  },
+
+  titleContainer: {
     padding: 20,
+    paddingTop: 0,
   },
 
   title: {
     fontSize: 24,
     fontWeight: "500",
   },
+
+  description: {},
+
   price: {
     color: colors.secondary,
     fontWeight: "bold",
     marginVertical: 10,
   },
-  userContainer: {
-    marginVertical: 40,
-  },
 });
-
-/* Description: This is the listing details screen for each listing which
-should display the details of each listing on press from the listings screen
-*/
