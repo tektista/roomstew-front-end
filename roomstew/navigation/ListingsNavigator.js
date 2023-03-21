60492daa7629fa9bb279c58cd7304408d7b81019
tektista <jnl.bautista.cs@gmail.com>
2023-03-21 19:10:26 +0000

    change listings navigator to use new screen compon

--- roomstew/navigation/ListingsNavigator.js
+++ roomstew/navigation/ListingsNavigator.js
@@ -1,18 +1,55 @@
 import React from "react";
 
-import ListingsScreen from "../screens/ListingsScreen";
-import ListingDetailsScreen from "../screens/listing_details/ListingDetailsScreen";
-import ListingDetailsShowMoreDetailsScreen from "../screens/listing_details/ListingDetailsShowMoreDetailsScreen";
-import ListingDetailsShowMoreDescScreen from "../screens/listing_details/ListingDetailsShowMoreDescScreen";
-import ListingDetailsRoomDetailsScreen from "../screens/listing_details/ListingRoomDetailsScreen";
+// import ListingsScreen from "../screens/ListingsScreen";
+// import ListingDetailsScreen from "../screens/listing_details/ListingDetailsScreen";
+// import ListingDetailsShowMoreDetailsScreen from "../screens/listing_details/ListingDetailsShowMoreDetailsScreen";
+// import ListingDetailsShowMoreDescScreen from "../screens/listing_details/ListingDetailsShowMoreDescScreen";
+// import ListingDetailsRoomDetailsScreen from "../screens/listing_details/ListingRoomDetailsScreen";
+// import ListingDetailsMapScreen from "../screens/listing_details/ListingDetailsMapScreen";
+
+import ListingsResultsScreen from "../screens/listing_details_search/ListingsResultsScreen";
+import ListingMapScreen from "../screens/listing_details_search/ListingMapScreen";
+import ListingDetailsScreen from "../screens/listing_details_search/ListingDetailsScreen";
+import ListingRoomDetailsScreen from "../screens/listing_details_search/ListingRoomDetailsScreen";
+import ListingShowMoreDescriptionScreen from "../screens/listing_details_search/ListingShowMoreDescriptionScreen";
+import ListingShowMoreDetailsScreen from "../screens/listing_details_search/ListingShowMoreDetailsScreen";
 
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 const Stack = createNativeStackNavigator();
 
 const ListingsNavigator = () => (
   <Stack.Navigator>
-    <Stack.Screen name="Listings" component={ListingsScreen} />
-    <Stack.Screen name="ListingDetails" component={ListingDetailsScreen} />
+    <Stack.Screen
+      name="ListingResultsScreen"
+      component={ListingsResultsScreen}
+    />
+
+    <Stack.Screen
+      name="ListingDetailsScreen"
+      component={ListingDetailsScreen}
+    />
+
+    <Stack.Screen name="ListingMapScreen" component={ListingMapScreen} />
+
+    <Stack.Screen
+      name="ListingRoomDetailsScreen"
+      component={ListingRoomDetailsScreen}
+    />
+
+    <Stack.Screen
+      name="ListingShowMoreDescriptionScreen"
+      component={ListingShowMoreDescriptionScreen}
+    />
+
+    <Stack.Screen
+      name="ListingShowMoreDetailsScreen"
+      component={ListingShowMoreDetailsScreen}
+    />
+    {/* <Stack.Screen name="Listings" component={ListingsScreen} /> */}
+    {/* 
+    <Stack.Screen name="ListingDetails" component={ListingDetailsScreen} /> */}
+
+    {/*     
     <Stack.Screen
       name="ListingDetailsShowMoreDetailsScreen"
       component={ListingDetailsShowMoreDetailsScreen}
@@ -25,6 +62,10 @@ const ListingsNavigator = () => (
       name="ListingDetailsRoomDetailsScreen"
       component={ListingDetailsRoomDetailsScreen}
     />
+    <Stack.Screen
+      name="ListingDetailsMapScreen"
+      component={ListingDetailsMapScreen}
+    /> */}
   </Stack.Navigator>
 );
 
