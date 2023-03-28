551e6137e29324a4e9f4e04f0e7e4a0f3054f663
tektista <jnl.bautista.cs@gmail.com>
2023-03-28 11:21:40 +0100

    add more filters to search screen

--- roomstew/screens/listing_details_search/ListingsSearchScreen.js
+++ roomstew/screens/listing_details_search/ListingsSearchScreen.js
@@ -1,25 +1,49 @@
 import { StyleSheet, View, ScrollView } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import * as Yup from "yup";
 
 import AppForm from "../../components/forms/AppForm";
 import ListItemPickerFormField from "../../components/forms/ListItemPickerFormField";
 import TextInputFormField from "../../components/forms/TextInputFormField";
+import DatePickerFormFieldField from "../../components/forms/DatePickerFormField";
 import FormSubmitButton from "../../components/forms/FormSubmitButton";
+import { getFormatedDate } from "react-native-modern-datepicker";
+import moment from "moment";
 
 import Icon from "../../components/Icon";
 import colors from "../../config/colors";
 import AppText from "../../components/AppText";
 import ListItemSeparator from "../../components/ListItemSeparator";
 
-const items = [{ label: "Any", value: "", subTitle: "Any" }];
-
+//item pickers for min rooms
+const minRoomPickerItems = [{ label: "Any", value: "", subTitle: "Any" }];
 for (let i = 1; i <= 12; i++) {
   const label = `${i} room${i > 1 ? "s" : ""} available`;
   const value = i;
   const subTitle = label;
-  items.push({ label, value, subTitle });
+  minRoomPickerItems.push({ label, value, subTitle });
 }
+
+const bathroomPickerItems = [{ label: "Any", value: "", subTitle: "Any" }];
+for (let i = 1; i <= 12; i++) {
+  const label = `${i} Bathroom${i > 1 ? "s" : ""}`;
+  const value = i;
+  const subTitle = label;
+  bathroomPickerItems.push({ label, value, subTitle });
+}
+
+const trueOrFalsePickerItems = [
+  { label: "Any", value: "", subTitle: "Any" },
+  { label: "Yes", value: 1, subTitle: "Yes" },
+  { label: "No", value: 0, subTitle: "No" },
+];
+
+const buildingTypePickerItems = [
+  { label: "Any", value: "", subTitle: "Any" },
+  { label: "Flat", value: 0, subTitle: "Flat" },
+  { label: "House", value: 1, subTitle: "House" },
+  { label: "Other", value: 2, subTitle: "Other" },
+];
 
 const validationSchema = Yup.object().shape({
   cityToSearch: Yup.string()
@@ -43,6 +67,9 @@ const validationSchema = Yup.object().shape({
 });
 
 const ListingsSearchScreen = ({ navigation }) => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState("No end date");
+
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
       <View style={{ paddingHorizontal: 20, paddingTop: 20 }}>
@@ -53,12 +80,34 @@ const ListingsSearchScreen = ({ navigation }) => {
         <AppForm
           initialValues={{
             cityToSearch: "",
-            minRoomsAvailable: 0,
-            minRent: 0,
-            maxRent: 0,
+            postcodeToSearch: "",
+            //added
+            dateAvailableBy: new Date(),
+
+            minRoomsAvailable: "",
+            minRent: "",
+            maxRent: "",
+            //added
+            maxDeposit: "",
+            isRoomFurnished: "",
+            isRoomEnsuite: "",
+            isFurnished: "",
+            hasLivingRoom: "",
+            bathroomCount: "",
+            hasHmo: "",
+            billsIncluded: "",
+            internetIncluded: "",
+            buildingType: "",
+            hasGarden: "",
+            hasParking: "",
           }}
           onSubmit={(values) => {
+            //TO DO: fix serializable log error
+            // values.dateAvailableBy = moment(values.dateAvailableBy).format(
+            //   "YYYY/MM/DD"
+            // );
             navigation.navigate("ListingsResultsScreen", { values });
+            console.log(values);
           }}
           validationSchema={validationSchema}
         >
@@ -74,6 +123,16 @@ const ListingsSearchScreen = ({ navigation }) => {
               textContentType="addressCity"
             />
 
+            <TextInputFormField
+              style={{ width: "80%" }}
+              autoCapitalize="none"
+              autocorrect={false}
+              keyboardType="default"
+              title="Postcode"
+              name="postCodeToSearch"
+              placeholder="e.g. SW1 2AA (optional)"
+            />
+
             <View
               style={{
                 flexDirection: "row",
@@ -86,9 +145,9 @@ const ListingsSearchScreen = ({ navigation }) => {
                 autoCapitalize="none"
                 autocorrect={false}
                 keyboardType="numeric"
-                title="min rent in £/month"
+                title="Min rent £ /month"
                 name="minRent"
-                placeholder="400 (optional)"
+                placeholder="500 (optional)"
                 textContentType="none"
               />
 
@@ -97,23 +156,180 @@ const ListingsSearchScreen = ({ navigation }) => {
                 autoCapitalize="none"
                 autocorrect={false}
                 keyboardType="numeric"
-                title="max rent in £/month"
+                title="Max rent £ /month"
                 name="maxRent"
-                placeholder="650 (optional)"
+                placeholder="1000 (optional)"
                 textContentType="none"
               />
             </View>
+            <TextInputFormField
+              width="45%"
+              autoCapitalize="none"
+              autocorrect={false}
+              keyboardType="numeric"
+              title="Max deposit"
+              name="maxDeposit"
+              placeholder="500 (optional)"
+              textContentType="none"
+            />
           </View>
+
+          <ListItemSeparator />
+          <AppText
+            style={{
+              fontSize: 20,
+              fontWeight: "bold",
+              paddingTop: 10,
+              paddingLeft: 10,
+            }}
+          >
+            Room Filters
+          </AppText>
+
+          <DatePickerFormFieldField
+            name="dateAvailableBy"
+            title="Available by"
+            IconComponent={
+              <Icon name="calendar" backgroundColor={colors.primary} />
+            }
+            //max should be
+            minDate={getFormatedDate(new Date(), "YYYY-MM-DD")}
+            maxDate={
+              endDate === "No end date"
+                ? getFormatedDate(
+                    new Date(
+                      startDate.getFullYear(),
+                      startDate.getMonth() + 6,
+                      startDate.getDate()
+                    ),
+                    "YYYY-MM-DD"
+                  )
+                : getFormatedDate(endDate, "YYYY-MM-DD")
+            }
+            onSelectItem={(value) => {
+              const newStartDate = new Date(value);
+              newStartDate.setMonth(newStartDate.getMonth());
+              setStartDate(newStartDate);
+            }}
+          />
 
           <ListItemSeparator />
 
           <ListItemPickerFormField
             name="minRoomsAvailable"
-            title="No. of available rooms"
-            items={items}
+            title="Min available rooms"
+            items={minRoomPickerItems}
             IconComponent={<Icon name="bed" backgroundColor={colors.primary} />}
           />
           <ListItemSeparator />
+
+          <ListItemPickerFormField
+            name="isRoomFurnished"
+            title="Room furnished"
+            items={trueOrFalsePickerItems}
+            IconComponent={
+              <Icon name="table-chair" backgroundColor={colors.primary} />
+            }
+          />
+          <ListItemSeparator />
+
+          <ListItemPickerFormField
+            name="isRoomEnsuite"
+            title="En-suite"
+            items={trueOrFalsePickerItems}
+            IconComponent={
+              <Icon name="toilet" backgroundColor={colors.primary} />
+            }
+          />
+
+          <ListItemSeparator />
+          <AppText
+            style={{
+              fontSize: 20,
+              fontWeight: "bold",
+              paddingTop: 10,
+              paddingLeft: 10,
+            }}
+          >
+            Listing Filters
+          </AppText>
+
+          <ListItemPickerFormField
+            name="isFurnished"
+            title="Property furnished"
+            items={trueOrFalsePickerItems}
+            IconComponent={
+              <Icon name="table-chair" backgroundColor={colors.primary} />
+            }
+          />
+          <ListItemSeparator />
+
+          <ListItemPickerFormField
+            name="hasLivingRoom"
+            title="Living room"
+            items={trueOrFalsePickerItems}
+            IconComponent={
+              <Icon name="sofa" backgroundColor={colors.primary} />
+            }
+          />
+          <ListItemSeparator />
+
+          <ListItemPickerFormField
+            name="bathroomCount"
+            title="Bathrooms"
+            items={bathroomPickerItems}
+            IconComponent={
+              <Icon name="toilet" backgroundColor={colors.primary} />
+            }
+          />
+
+          <ListItemSeparator />
+
+          <ListItemPickerFormField
+            name="hasHmo"
+            title="HMO"
+            items={trueOrFalsePickerItems}
+            IconComponent={
+              <Icon
+                name="account-multiple-check"
+                backgroundColor={colors.primary}
+              />
+            }
+          />
+          <ListItemSeparator />
+
+          <ListItemPickerFormField
+            name="billsIncluded"
+            title="Bills included"
+            items={trueOrFalsePickerItems}
+            IconComponent={
+              <Icon
+                name="home-lightning-bolt"
+                backgroundColor={colors.primary}
+              />
+            }
+          />
+          <ListItemSeparator />
+
+          <ListItemPickerFormField
+            name="internetIncluded"
+            title="Internet included"
+            items={trueOrFalsePickerItems}
+            IconComponent={
+              <Icon name="wifi" backgroundColor={colors.primary} />
+            }
+          />
+
+          <ListItemSeparator />
+
+          <ListItemPickerFormField
+            name="buildingType"
+            title="Building type"
+            items={buildingTypePickerItems}
+            IconComponent={
+              <Icon name="office-building" backgroundColor={colors.primary} />
+            }
+          />
 
           <View style={{ flexGrow: 1 }}></View>
 
--- roomstew/services/listingsService.js
+++ roomstew/services/listingsService.js
@@ -1,32 +1,93 @@
 import axios from "axios";
-import { Circle } from "react-native-maps";
+import moment from "moment";
 
 const baseURL = "http://localhost:3002/api/listings";
 // const baseURL = `https://nodejs-cleardb-project.herokuapp.com/api/listings`;
+
 const getAllListings = (
   offset,
   cityToSearch,
   minRoomsAvailable,
   minRent,
-  maxRent
+  maxRent,
+  postCodeToSearch,
+  dateAvailableBy,
+  maxDeposit,
+  isRoomFurnished,
+  isRoomEnsuite,
+  isFurnished,
+  hasLivingRoom,
+  bathroomCount,
+  hasHmo,
+  billsIncluded,
+  internetIncluded,
+  buildingType,
+  hasGarden,
+  hasParking
 ) => {
   let url = `${baseURL}/?offset=${offset}`;
 
   if (cityToSearch) {
     url += `&city=${cityToSearch}`;
   }
-  if (minRoomsAvailable) {
-    url += `&minRooms=${minRoomsAvailable}`;
+  if (postCodeToSearch) {
+    url += `&postcode=${postCodeToSearch}`;
   }
+
   if (minRent) {
     url += `&minRent=${minRent}`;
   }
   if (maxRent) {
     url += `&maxRent=${maxRent}`;
   }
-  console.log(url);
+  if (maxDeposit) {
+    url += `&maxDeposit=${maxDeposit}`;
+  }
+  if (dateAvailableBy) {
+    url += `&dateAvailable=${dateAvailableBy.toISOString()}`;
+  }
+  if (minRoomsAvailable) {
+    url += `&minRooms=${minRoomsAvailable}`;
+  }
+  if (isRoomFurnished !== "") {
+    url += `&isRoomFurnished=${Boolean(isRoomFurnished)}`;
+  }
+  if (isRoomEnsuite !== "") {
+    console.log("isRoomEnsuite", isRoomEnsuite);
+    url += `&isRoomEnsuite=${isRoomEnsuite}`;
+  }
+  if (isFurnished !== "") {
+    url += `&isFurnished=${Boolean(isFurnished)}`;
+  }
+  if (hasLivingRoom !== "") {
+    url += `&hasLivingRoom=${Boolean(hasLivingRoom)}`;
+  }
+  if (bathroomCount) {
+    url += `&bathroomCount=${bathroomCount}`;
+  }
+  if (hasHmo !== "") {
+    url += `&hasHmo=${hasHmo}`;
+  }
+  if (billsIncluded !== "") {
+    url += `&billsIncluded=${Boolean(billsIncluded)}`;
+  }
+  if (internetIncluded !== "") {
+    url += `&internetIncluded=${Boolean(internetIncluded)}`;
+  }
+
+  if (buildingType !== "") {
+    url += `&buildingType=${buildingType}`;
+  }
+  if (hasGarden !== "") {
+    url += `&hasGarden=${Boolean(hasGarden)}`;
+  }
+  if (hasParking !== "") {
+    url += `&hasParking=${Boolean(hasParking)}`;
+  }
+
   return axios.get(url);
 };
+
 const getAllListingsByListingIds = (offset) => {
   return axios.get(`${baseURL}/save/?offset=${offset}`);
 };
